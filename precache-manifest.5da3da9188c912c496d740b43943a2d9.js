7c1ae66fcb7bb37d7d1e2750c2b60e34d558a089
kidaqrus <adikwusule@gmail.com>
2019-11-06 07:53:49 +0100

    Updates

---
+++ precache-manifest.5da3da9188c912c496d740b43943a2d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0527e42534215271103",
+    "url": "/myPort/static/js/runtime~main.a0527e42.js"
+  },
+  {
+    "revision": "8ed5a7a0cea448447657",
+    "url": "/myPort/static/js/main.8ed5a7a0.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/myPort/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "8ed5a7a0cea448447657",
+    "url": "/myPort/static/css/main.32138097.chunk.css"
+  },
+  {
+    "revision": "3ef102a71ca870e84fb205feec4aa656",
+    "url": "/myPort/index.html"
+  }
+];
